b70992813d2b2c3cabb00e1bae1b1fbfad60052b
lemancebrayilova <cbrayilovalman33@gmail.com>
2025-08-17 18:46:21 +0300

    Init client logos carousel

--- js/main.js
+++ js/main.js
@@ -12,3 +12,25 @@ window.onscroll = function() {
 // Nav Hide
 
 // Owl Carousel
+$(document).ready(function() {
+    $(".client-slider-section").owlCarousel({
+        items: 4,
+        loop: true,
+        nav:false,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        autoplayHoverPause: true,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 2
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 6
+            }
+        }
+    });
+});
